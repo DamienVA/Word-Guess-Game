6b92bc2f43dee09b55f9ba8952db62133d75088c
Damien Aponte <damienapo@gmail.com>
2019-08-24 06:33:00 -0400

    rules of the game finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,14 @@
-var underscores = []
-
-word = ["toyota", "honda", "nissan", "mazda", "subaru"]
-
+var underscores = [];
+var word = ["toyota", "honda", "nissan", "mazda", "subaru"];
+var wrong = [];
+var right = [];
 var random = Math.floor(Math.random() * word.length);
 
-chosenWord = word[random];
+var underDoc = document.getElementsByClassName('underscores');
 
+chosenWord = word[random];
 console.log(chosenWord);
+
 function createUnderscores() {
     for (let i = 0; i < chosenWord.length; i++) {
         underscores.push("_");
@@ -19,8 +21,19 @@ document.onkeyup = function (event) {
 
     var ltr = event.key.toLowerCase();
 
-    if(chosenWord.indexOf(ltr) > -1) {
-        
+    if (chosenWord.indexOf(ltr) > -1) {
+        right.push(ltr);
+        underscores[chosenWord.indexOf(ltr)] = ltr;
+        console.log(underscores);
+        if (underscores.join('') == chosenWord) {
+            alert('You Win!');
+        }
     }
-};
+    else{
+    wrong.push(ltr);
+    console.log(wrong);
+}};
+
+
+
 
