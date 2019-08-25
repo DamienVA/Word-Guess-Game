79f8632e7f8dc147ef29ea6aa6eb6acd394ccfed
Damien Aponte <damienapo@gmail.com>
2019-08-25 07:32:36 -0400

    changed some rules but couldn't finish in time

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,7 @@ var wins = 0;
 
 var underDoc = document.getElementsByClassName('underscores');
 var wrongDoc = document.getElementsByClassName('ltrGuessed');
+var winDoc = document.getElementsByClassName('wins');
 
 chosenWord = word[random];
 console.log(chosenWord);
@@ -16,25 +17,43 @@ function createUnderscores() {
         underscores.push("_");
     }
 };
-
+//TRIED TO FIGURE OUT HOW TO FIX ISSUE WITH COMMENTED OUT CODE
 document.onkeyup = function (event) {
 
     var ltr = event.key.toLowerCase();
 
-    if (chosenWord.indexOf(ltr) > -1) {
-        right.push(ltr);
-        underscores[chosenWord.indexOf(ltr)] = ltr;
-        underDoc.innerHTML = underscores.join(' ');
-        console.log(underscores);
-        if (underscores.join('') == chosenWord) {
-            alert('You Win!');
+    for (var i = 0; i < chosenWord.length; i++) {
+        
+
+        if (ltr === chosenWord[i] && underscores === '_') {
+            
+            right.push(ltr);
+            underscores[chosenWord.indexOf(ltr)] = ltr;
+            console.log(underscores);
+            
         }
+        
     }
-    else {
-        wrong.push(ltr);
-        console.log(wrong);
-    }
+//THIS CODE WORKS BUT ONLY WITH WORDS THAT DON'T REPEAT LETTERS
+    // if (chosenWord.indexOf(ltr) > -1) {
+    //     right.push(ltr);
+    //     underscores[chosenWord.indexOf(ltr)] = ltr;
+    //     underDoc.innerHTML = underscores.join(' ');
+    //     console.log(underscores);
+    //     if (underscores.join('') == chosenWord) {
+    //         alert('You Win!');
+    //     }
+    // }
+    // else {
+    //     wrong.push(ltr);
+    //     console.log(wrong);
+    // }
 };
+
+if(underscores.join('') == chosenWord) {
+    wins += 1;
+    winsDoc[0].innerHTML = wins.toString();
+}
 
 createUnderscores();
 console.log(underscores);
